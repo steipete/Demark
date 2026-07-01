5b150310ca4d6bca3546b1064f6493bc7a7befa8
Peter Steinberger <steipete@gmail.com>
2026-07-01 15:53:10 +0100

    build: update Turndown to 7.2.4 (#6)
    * build: update Turndown to 7.2.4
    
    * fix: bound Turndown initialization
    
    * test: avoid wall-clock timeout assertion

--- Tests/DemarkTests/DemarkServiceBasicsTests.swift
+++ Tests/DemarkTests/DemarkServiceBasicsTests.swift
@@ -47,4 +47,42 @@ struct DemarkServiceBasicsTests {
         #expect(markdown.contains("[our website](https://example.com)"))
         #expect(markdown.contains("Visit"))
     }
+
+    @Test("Link and image attributes are escaped")
+    func linkAndImageAttributesAreEscaped() async throws {
+        let service = Demark()
+        let html = """
+        <a href="https://example.com/foo bar" title="Hello &quot;World&quot;">Link</a>
+        <img src="logo.png?(query)" alt="_img_ *with* [alt]">
+        """
+
+        let markdown = try await service.convertToMarkdown(html)
+
+        #expect(markdown.contains(#"[Link](<https://example.com/foo bar> "Hello \"World\"")"#))
+        #expect(markdown.contains(#"![\_img\_ \*with\* \[alt\]](logo.png?\(query\))"#))
+    }
+
+    #if canImport(WebKit)
+        @Test("Turndown initialization navigation is time-bounded")
+        func initializationNavigationIsTimeBounded() async {
+            var delegate: InitializationNavigationDelegate?
+            var didRunTimeout = false
+
+            do {
+                try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+                    delegate = InitializationNavigationDelegate(
+                        continuation: continuation,
+                        timeout: .milliseconds(10),
+                        onTimeout: { didRunTimeout = true }
+                    )
+                }
+                Issue.record("Expected initialization timeout")
+            } catch {
+                #expect(error is DemarkError)
+            }
+
+            #expect(didRunTimeout)
+            _ = delegate
+        }
+    #endif
 }
